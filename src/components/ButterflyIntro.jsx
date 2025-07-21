9aa77c52a06f81e8498d7ab2efe12d969884a141
gemayc <gemita284@hotmail.com>
2025-07-21 11:40:48 +0200

    style: update font colors and add spacing between header and navbar(#15)

--- src/components/ButterflyIntro.jsx
+++ src/components/ButterflyIntro.jsx
@@ -5,23 +5,23 @@ import introbutterflyg from "../assets/img/intro-butterfly-g.png";
 const ButterflyIntro = () => {
     return (
          <>
-              <header className="text-center mb-8">
+              <header className="text-center mb-7 mt-8">
                 <h1 
                   className=" font-segoe text-mint-green-700 text-3xl sm:text-4xl md:text-5xl font-bold mb-4 px-4">Descubre las {" "} <strong className="bg-gradient-to-r from-[#9cc7ab] via-[#abbf60]
                to-[#957112] bg-clip-text text-transparent drop-shadow-md"> Mariposas</strong> Asiáticas</h1>
               </header>
               <section
                 
-                className=" font-segoe flex flex-col md:flex-row justify-center items-center gap-8 p-8">
+                className="flex flex-col md:flex-row justify-center items-center gap-8 p-8">
                 <div className="flex justify-center items-center">
                   <img src={introbutterflyg} alt="Mariposa asiática" className="max-w-[250px] md:max-w-[400px] lg:max-w-[500px] h-auto animate-flotar" />
                 </div>
         
                 <div className="max-w-2xl order-1 md:order-2">
                   <div className="bg-white p-6 md:p-8 rounded-2xl shadow-2xl text-center md:text-left">
-                    <p className="text-sm md:text-base leading-relaxed opacity-0 animate-fade-in animation-delay-500">
+                    <p className="font-segoe text-mint-green-700 text-sm md:text-base leading-relaxed opacity-0 animate-fade-in animation-delay-500">
                       Las mariposas desempeñan un{" "}
-                      <strong className="text-green-700">papel fundamental como polinizadores.</strong>
+                      <strong className="text-lime-400">papel fundamental como polinizadores.</strong>
                       <br />
                       <br />
                       Al buscar néctar para alimentarse, sus grandes alas les ayudan a transferir
@@ -29,7 +29,7 @@ const ButterflyIntro = () => {
                       de las flores silvestres.
                       <br />
                       <br />
-                      <strong className="text-green-700">
+                      <strong className="text-lime-400">
                         Asia es un continente con una extraordinaria riqueza en diversidad de mariposas diurnas
                       </strong>, donde se pueden encontrar más de 500 especies, muchas de ellas destacadas
                       por sus vibrantes colores y gráciles formas.
@@ -40,7 +40,7 @@ const ButterflyIntro = () => {
                       y la producción de alimentos.
                       <br />
                       <br />
-                      <strong className="text-green-700">
+                      <strong className="text-lime-400">
                         El mantenimiento de una amplia variedad de mariposas y otros polinizadores
                         es esencial para la salud ambiental y la producción de alimentos.
                       </strong>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 <>
 <ButterflyIntro/>
 <section className="p-4">
-  <h2 style={{ fontFamily: "var(--font-segoe)" }} className="text-1xl sm:text-2xl md:text-3xl font-bold mb-4 px-4">Mapa de observaciones</h2>
+  <h2 className=" font-segoe text-mint-green-700 text-1xl sm:text-2xl md:text-3xl font-bold mb-4 px-4">Mapa de observaciones</h2>
 
   {/* <MapaINaturalist/> */}
 </section>
