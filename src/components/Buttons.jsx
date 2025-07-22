9607fa69732cae6fa6aa58a35c8217e74c5a2f5d
marianyarj <marianyarj@gmail.com>
2025-07-22 16:32:14 +0200

    style: add primary button styles (#10)

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
 
-function Buttons({ btnColor, btnColorHover, btnTextColor, btnText, }) {
+function Buttons({ styleType = "primary", text, className = "" }) {
+    const base = "py-2 px-4 rounded-lg font-segoe font-semibold cursor-pointer border-2";
+    const styles = {
+        primary: "bg-lime-green text-mint-green-700 border-mint-green-600 outline hover:bg",
+        secondary: "bg-white",
+        tertiary: "bg-red",
+        quaternary: "bg-red",
+        quinary: "bg-red"
+    }
+
     return (
-        <button className={`bg-${btnColor} text-${btnTextColor} text-white font-bold py-2 px-4 rounded`}>{btnText}</button>
+        <button className={`${className} ${base} ${styles[styleType]}`}>  {text} </button>
     )
 }
 
