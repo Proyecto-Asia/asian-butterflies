b1a1f3aff7a71b0afb89362c6647dfb56e8c55b3
marianyarj <marianyarj@gmail.com>
2025-07-23 13:24:40 +0200

    style: adjust navbar layout (#9)

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,10 +6,10 @@ function NavBar() {
     return (
         <>
             <nav
-                className={`bg-mint-green-700 text-white font-segoe p-6 pl-2 flex flex-row justify-between items-center font-bold 
-                ${isOpen ? "rounded-b-none" : "rounded-b-4xl"} md:rounded-b-4xl`}>
-                <h1 className=" ps-6 text-2xl">Asian Butterflies</h1>
-                <button className="md:hidden text-2xl px-4 py-2 hover:bg-mint-green-800 rounded-xl" onClick={() => setIsOpen(!isOpen)}>{isOpen ? (
+                className={`relative z-20 bg-mint-green-700 text-white font-segoe p-6 pl-2 flex flex-row justify-between items-center font-bold mx-6 xl:mx-50 rounded-2xl
+                ${isOpen ? "rounded-b-none" : "rounded-2xl"} md:rounded-2xl`}>
+                <Link to="/" className=" ps-6 text-2xl">Asian Butterflies</Link>
+                <button className="md:hidden text-2xl px-4 py-2 hover:bg-mint-green-800 rounded-xl cursor-pointer" onClick={() => setIsOpen(!isOpen)}>{isOpen ? (
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         height="24px"
@@ -35,7 +35,7 @@ function NavBar() {
                 </ul>
             </nav>
             {isOpen && (
-                <ul className="md:hidden absolute top-22 left-0 right-0 flex flex-col text-lg z-50 bg-mint-green-700 text-white font-segoe font-bold rounded-b-4xl pt-4 pb-4">
+                <ul className="md:hidden mx-6 absolute top-22 left-0 right-0 flex flex-col text-lg z-50 bg-mint-green-700 text-white font-segoe font-bold rounded-2xl pt-4 pb-4">
                     <li><Link to="/" onClick={() => setIsOpen(false)} className="active:bg-mint-green-800 hover:bg-mint-green-800 block w-full text-center px-4 py-2 rounded-4xl">Inicio</Link></li>
                     <li><Link to="/ButterflyGrid" onClick={() => setIsOpen(false)} className="active:bg-mint-green-800 hover:bg-mint-green-800 block w-full text-center px-4 py-2 rounded-4xl">Galería</Link></li>
                     <li><Link to="/newbutterfly" onClick={() => setIsOpen(false)} className="active:bg-mint-green-800 hover:bg-mint-green-800 block w-full text-center px-4 py-2 rounded-4xl">Crear nueva</Link></li>
