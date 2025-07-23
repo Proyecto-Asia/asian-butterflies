09cdc84efb1506cceb306a3d8e9f411d6afda6a3
Larysa Ambartsumian <hi.ambalari@gmail.com>
2025-07-23 14:03:20 +0300

    edited footer

--- src/pages/ButterflyDetail.jsx
+++ src/pages/ButterflyDetail.jsx
@@ -1,9 +1,29 @@
-function ButterflyDetail (){
+import { getOneButterfly } from "../services/ButterflyServices";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+function ButterflyDetail() {
+  const { id } = useParams();
+  const [butterfly, setButterfly] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const butterflyData = await getOneButterfly(id);
+        setButterfly(butterflyData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
 return(
 
     <>
-
-    </>
+      <h1>Hola</h1>
+      </>
 );
 
 }
