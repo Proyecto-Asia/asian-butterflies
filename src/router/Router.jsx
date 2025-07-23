6e065360a0c398b346ef44ca7603eea34493c699
olgararo <olgararo1402@gmail.com>
2025-07-23 14:58:02 +0200

    feature: add butterflygrid path for Galeria page

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -3,7 +3,7 @@ import CreateButterfly from "../pages/CreateButterfly";
 import EditedButterfly from "../pages/EditedButterfly";
 import ButterflyDetail from "../pages/ButterflyDetail";
 import Layout from "../layout/Layout";
-// import Home from "../pages/Home";
+import Home from "../pages/Home";
 import Contact from "../pages/Contact";
 import ButterflyGrid from "../pages/ButterflyGrid";
 
@@ -12,10 +12,10 @@ const routerButterfly = createBrowserRouter([
     path: "/",
     element: <Layout />,
     children: [
-     /* {
+     {
         index: true,
         element: <Home />,
-      },*/
+      },
       {
         path: "/newbutterfly",
         element: <CreateButterfly />,
@@ -33,7 +33,7 @@ const routerButterfly = createBrowserRouter([
         element: <Contact />,
       },
       {
-        path: "/ButterflyGrid",
+        path: "/butterflygrid",
         element: <ButterflyGrid />,
       },
     ],
